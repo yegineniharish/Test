ad5d51d1ecc350e322d0d1af54b1974e92e2426d
harish <yegineniharish@gmail.com>
2023-03-10 17:10:36 +0000

    Adding class

---
+++ SeleniumFramework/src/test/java/com/projectName/base/NewTestClasss.java
@@ -0,0 +1,14 @@
+package com.projectName.base;
+
+import org.testng.annotations.Test;
+
+public class NewTestClasss {
+	
+	@Test
+	public void test2(){
+		
+		System.out.println("second class");
+	
+	}
+
+}
